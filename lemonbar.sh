22e783709780dc9b53f45e50452c291f313763e1
replaceits <sharpcut2231@gmail.com>
2017-02-26 12:28:07 -0500

    Add script

---
+++ lemonbar.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+
+Clock(){
+	DATE=$(date "+%m.%d.%y")
+	TIME=$(date "+%I:%M")
+
+	echo -e -n "\uf073 ${DATE} \uf017 ${TIME}"
+}
+
+ActiveWindow(){
+	echo -n $(xdotool getwindowfocus getwindowname)
+}
+
+Battery() {
+	BATTACPI=$(acpi --battery)
+        BATPERC=$(echo $BATTACPI | cut -d, -f2)
+
+	if [[ $BATTACPI == *"100%"* ]]
+        then
+                echo "%{T2}✓%{T-}$BATPERC"
+	elif [[ $BATTACPI == *"Discharging"* ]]
+	then
+		echo "$BATPERC"
+	elif [[ $BATTACPI == *"Charging"* && $BATTACPI != *"100%"* ]]
+	then
+		echo "$BATPERC"
+	elif [[ $BATTACPI == *"Unknown"* ]]
+	then
+		echo "$BATPERC"
+	fi
+}
+
+Wifi(){
+	WIFISTR=$( iwconfig wlp1s0 | grep "Link" | sed 's/ //g' | sed 's/LinkQuality=//g' | sed 's/\/.*//g')
+	if [ ! -z $WIFISTR ] ; then
+		WIFISTR=$(( ${WIFISTR} * 100 / 70))
+		ESSID=$(iwconfig wlp1s0 | grep ESSID | sed 's/ //g' | sed 's/.*://' | cut -d "\"" -f 2)
+		if [ $WIFISTR -ge 1 ] ; then
+			echo -e "\uf1eb ${ESSID} ${WIFISTR}%"
+		fi
+	fi
+}
+
+Sound(){
+	VOL=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master) | sed 's/%//g')
+	if [ $VOL -ge 85 ] ; then
+		echo -e "\uf028 ${VOL}%"
+	elif [ $VOL -ge 50 ] ; then
+		echo -e "\uf027 ${VOL}%"
+	else
+		echo -e "\uf026 ${VOL}%"
+	fi
+}
+
+while true; do
+	echo -e "%{l}" "%{c}$(ActiveWindow)" "%{r}$(Wifi)  $(Sound)  $(Clock)"
+	sleep 0.1s
+done
