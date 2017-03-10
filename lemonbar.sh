3ebd1605c71698703edd193932e4a4c8b927b299
replaceits <sharpcut2231@gmail.com>
2017-03-10 13:20:39 -0500

    Add battery stats

--- lemonbar.sh
+++ lemonbar.sh
@@ -13,20 +13,37 @@ ActiveWindow(){
 
 Battery() {
 	BATTACPI=$(acpi --battery)
-        BATPERC=$(echo $BATTACPI | cut -d, -f2)
+    BATPERC=$(echo $BATTACPI | cut -d, -f2 | tr -d '[:space:]')
 
 	if [[ $BATTACPI == *"100%"* ]]
-        then
-                echo "%{T2}✓%{T-}$BATPERC"
+    then
+        echo -e -n "\uf00c $BATPERC"
 	elif [[ $BATTACPI == *"Discharging"* ]]
 	then
-		echo "$BATPERC"
+		BATPERC=${BATPERC::-1}
+		if [ $BATPERC -le "10" ]
+		then
+			echo -e -n "\uf244"
+		elif [ $BATPERC -le "25" ]
+		then
+			echo -e -n "\uf243"
+		elif [ $BATPERC -le "50" ]
+		then
+			echo -e -n "\uf242"
+		elif [ $BATPERC -le "75" ]
+		then
+			echo -e -n "\uf241"
+		elif [ $BATPERC -le "100" ]
+		then
+			echo -e -n "\uf240"
+		fi
+		echo -e " $BATPERC%"
 	elif [[ $BATTACPI == *"Charging"* && $BATTACPI != *"100%"* ]]
 	then
-		echo "$BATPERC"
+		echo -e "\uf0e7 $BATPERC"
 	elif [[ $BATTACPI == *"Unknown"* ]]
 	then
-		echo "$BATPERC"
+		echo -e "$BATPERC"
 	fi
 }
 
@@ -53,6 +70,6 @@ Sound(){
 }
 
 while true; do
-	echo -e "%{l}" "%{c}$(ActiveWindow)" "%{r}$(Wifi)  $(Sound)  $(Clock)"
+	echo -e "%{l}" "%{c}$(ActiveWindow)" "%{r}$(Wifi)  $(Battery)  $(Sound)  $(Clock)"
 	sleep 0.1s
 done
