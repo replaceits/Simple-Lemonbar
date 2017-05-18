4abe03007f36fd4bb54dc629a47ada21aec99f9c
replaceits <sharpcut2231@gmail.com>
2017-05-18 14:30:33 -0400

    Add display for muted volume

--- lemonbar.sh
+++ lemonbar.sh
@@ -13,11 +13,11 @@ ActiveWindow(){
 
 Battery() {
 	BATTACPI=$(acpi --battery)
-    BATPERC=$(echo $BATTACPI | cut -d, -f2 | tr -d '[:space:]')
+	BATPERC=$(echo $BATTACPI | cut -d, -f2 | tr -d '[:space:]')
 
 	if [[ $BATTACPI == *"100%"* ]]
-    then
-        echo -e -n "\uf00c $BATPERC"
+	then
+		echo -e -n "\uf00c $BATPERC"
 	elif [[ $BATTACPI == *"Discharging"* ]]
 	then
 		BATPERC=${BATPERC::-1}
@@ -59,13 +59,18 @@ Wifi(){
 }
 
 Sound(){
-	VOL=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master) | sed 's/%//g')
-	if [ $VOL -ge 85 ] ; then
-		echo -e "\uf028 ${VOL}%"
-	elif [ $VOL -ge 50 ] ; then
-		echo -e "\uf027 ${VOL}%"
+	NOTMUTED=$( amixer sget Master | grep "\[on\]" )
+	if [[ ! -z $NOTMUTED ]] ; then
+		VOL=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master) | sed 's/%//g')
+		if [ $VOL -ge 85 ] ; then
+			echo -e "\uf028 ${VOL}%"
+		elif [ $VOL -ge 50 ] ; then
+			echo -e "\uf027 ${VOL}%"
+		else
+			echo -e "\uf026 ${VOL}%"
+		fi
 	else
-		echo -e "\uf026 ${VOL}%"
+		echo -e "\uf026 M"
 	fi
 }
 
